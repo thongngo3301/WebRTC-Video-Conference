30f49069d550ff14379843498fd65947ec6ac25a
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2018-11-29 13:02:09 +0700

    resolve conflict

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const path = require('path');
 const serverPort = process.env.OPENSHIFT_NODEJS_PORT || 1337
 const serverIpAddress = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
 const socketIoServer = '127.0.0.1';
+const kurento_uri = 'ws://localhost:8888/kurento';
 
 ////////////////////////////////////////////////
 // SETUP SERVER
@@ -37,6 +38,7 @@ const server = app.listen(serverPort, serverIpAddress, function () {
 });
 
 const io = require('socket.io').listen(server);
+
 
 ////////////////////////////////////////////////
 // EVENT HANDLERS
@@ -77,6 +79,97 @@ io.sockets.on('connection', function (socket) {
 			socket.emit('joined', room);
 		}
 	});
+	
+	socket.on('kurento create offer', function(data) {
+		let sdpOffer = data.sdpOffer;
+		kurento(kurento_uri, (error, kurentoClient) => {
+			kurentoClient.create('MediaPipeline', (error, pipeline) => {
+				if (error) {
+					console.log(error);
+				}
+				else {
+					socket.pipeline = pipeline;
+					pipeline.create('WebRtcEndpoint', (error, webRtcEndpoint) => {
+						if (error) {
+							console.log(error);
+						}
+						else {
+							// Define handler received icecandidate
+							webRtcEndpoint.on('OnIceCandidate', (event) => {
+								// received icecandidate from server and send to client
+								socket.emit('icecandidate exchange', JSON.stringify(event.candidate));
+							});
+
+							// Define handler create sdpAnswer
+							webRtcEndpoint.processOffer(sdpOffer, (error, sdpAnswer) => {
+								if (error) {
+									console.log(error);
+								}
+								else {
+									socket.emit('kurento answer offer', JSON.stringify(sdpAnswer));
+								}
+							});
+
+							// Search icecandidate available
+							webRtcEndpoint.gatherCandidates(error => {
+								if (error) {
+									console.log(error);
+								}
+							});
+
+							socket.webRtcEndpoint = webRtcEndpoint;
+						}
+					});
+				}
+			});
+		});
+	});
+
+	// create connect many to many
+	socket.on('start stream', function (data) {
+		let sdpOffer = data.sdpOffer;		
+		let otherClients = io.sockets.clients(socket.room);
+		
+		otherClients.forEach(client => {
+			// As presenter
+			socket.pipeline.create('WebRtcEndpoint', (error, webRtcEndpoint) => {
+				if (error) {
+					console.log(error);
+				}
+				else {
+					if (client.presenters === undefined) {
+						client.presenters = [];
+					}
+					client.presenters.push(webRtcEndpoint);
+					webRtcEndpoint.on('IceCandidate', event => {
+						socket.emit('icecandidate exchange', JSON.stringify(event.candidate));
+					});
+					webRtcEndpoint.processOffer(sdpOffer, (error, sdpAnswer) => {
+						socket.webRtcEndpoint.connect(webRtcEndpoint, error => {
+							if (error) {
+								console.log(error);
+							}
+							else {
+								client.emit('candidate exchange', JSON.stringify(event.candidate));
+							}
+						})
+					})
+				}
+			});
+
+			// As viewer
+			client.pipeline.create('WebRtcEndpoint', (error, webRtcEndpoint) => {
+				if (error) {
+					console.log(error);
+				}
+				else {
+					
+				}
+			})
+		});
+
+	});
+
 
 	// Setup a communication channel (namespace) to communicate with a given participant (participantID)
 	function configNameSpaceChannel(participantID) {
